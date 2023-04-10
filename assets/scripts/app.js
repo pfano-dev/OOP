78523ff8d98f8a7b9a9e9b8e6fac381e906fe8b7
muleya pfano <79614241+pfano-dev@users.noreply.github.com>
2023-04-10 17:15:13 +0200

    spliting  classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,42 +14,66 @@ class Product{
   }
 
 }
+class ProductItem{
+  constructor(product){
+    this.product = product;
+  }
 
+  render(){
 
-
-const productsList ={
-
-  products : [
-    new Product("A Pallow","https://www.vencasa.co.za/wp-content/uploads/2021/06/comfort-cloud_0000_Pillow-Hug_Comfort-Cloud-pillow_Original-lux-1.jpg",19.99,"a soft pillow"),
-new Product("A carpet","https://assets-global.website-files.com/5e96bcc5e521f9c598e4a72d/606321f4baf4ac3ed32f4af1_belgotex-carpets-conqueror-hero.jpeg",19.99,"A carpet which you might like or not")
-   ],
-   render(){
-    const renderHook = document.getElementById("app");
-    const prodList = document.createElement("ul");
-    prodList.className = 'product-list';
-
-    for(const prod of this.products){
-      const prodEl = document.createElement("li");
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-
-        <div>
-          <img src ="${prod.imageUrl}" alt = "${prod.title}">
-          <div class = "product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to cart </button>
-          </div>
-        </div>
-
-      `;
-      prodList.append(prodEl);
-    }
+    const prodEl = document.createElement("li");
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
   
-    renderHook.append(prodList);
-   }
- }
+          <div>
+            <img src ="${this.product.imageUrl}" alt = "${this.product.title}">
+            <div class = "product-item__content">
+              <h2>${this.product.title}</h2>
+              <h3>\$${this.product.price}</h3>
+              <p>${this.product.description}</p>
+              <button>Add to cart </button>
+            </div>
+          </div>
+  
+        `;
+    return prodEl
+  }
 
- productsList.render();
- 
+}
+
+class ProductList {
+
+  products = [
+    new Product("A Pallow","https://www.vencasa.co.za/wp-content/uploads/2021/06/comfort-cloud_0000_Pillow-Hug_Comfort-Cloud-pillow_Original-lux-1.jpg",19.99,"a soft pillow"),
+    new Product("A carpet","https://assets-global.website-files.com/5e96bcc5e521f9c598e4a72d/606321f4baf4ac3ed32f4af1_belgotex-carpets-conqueror-hero.jpeg",19.99,"A carpet which you might like or not"),
+    
+   
+   ];
+
+   constructor(){}
+
+    render(){
+      const renderHook = document.getElementById("app");
+      const prodList = document.createElement("ul");
+      prodList.className = 'product-list';
+  
+      for(const prod of this.products){
+        const productItem = new ProductItem(prod);
+        const prodEl = productItem.render();
+        prodList.append(prodEl);
+      }
+    
+      renderHook.append(prodList);
+     }
+
+
+
+
+   }
+
+
+
+
+const newProductList = new ProductList();
+
+newProductList.render();
