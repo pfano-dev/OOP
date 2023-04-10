e9217e66e876c851a2e314c124e1e9d25028e9e5
muleya pfano <79614241+pfano-dev@users.noreply.github.com>
2023-04-10 16:33:03 +0200

    adding a class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,27 @@
+class Product{
+
+  title = "DEFAULT";
+  imageUrl;
+  description;
+  price;
+
+  constructor(title, image, desc, price){
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+    
+  }
+
+}
+
+
+
 const productsList ={
 
   products : [
-    {
-      title:"A Pallow",
-      imageUrl: "https://www.vencasa.co.za/wp-content/uploads/2021/06/comfort-cloud_0000_Pillow-Hug_Comfort-Cloud-pillow_Original-lux-1.jpg",
-      price: 19.99,
-      description: "a soft pillow"
-    },
-    {
-      title:"A carpet",
-      imageUrl: "https://assets-global.website-files.com/5e96bcc5e521f9c598e4a72d/606321f4baf4ac3ed32f4af1_belgotex-carpets-conqueror-hero.jpeg",
-      price: 19.99,
-      description: "A carpet which you might like or not"
-    }
+    new Product("A Pallow","https://www.vencasa.co.za/wp-content/uploads/2021/06/comfort-cloud_0000_Pillow-Hug_Comfort-Cloud-pillow_Original-lux-1.jpg",19.99,"a soft pillow"),
+new Product("A carpet","https://assets-global.website-files.com/5e96bcc5e521f9c598e4a72d/606321f4baf4ac3ed32f4af1_belgotex-carpets-conqueror-hero.jpeg",19.99,"A carpet which you might like or not")
    ],
    render(){
     const renderHook = document.getElementById("app");
@@ -43,4 +52,4 @@ const productsList ={
  }
 
  productsList.render();
- productsList.render();
+ 
